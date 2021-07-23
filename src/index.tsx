22ecb2ecd8a472bb14f0b2333d351bc567fc6658
Renato Ribeiro <renato_ribeiro33@hotmail.com>
2021-07-23 18:02:15 -0300

    List transactions

--- src/index.tsx
+++ src/index.tsx
@@ -5,19 +5,42 @@ import App from './App';
 
 createServer({
   models: {
-    transaction : Model,
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 5000,
+          createDate: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createDate: new Date('2021-02-14 11:00:00'),
+        },
+      ]
+    })
   },
 
   routes() {
-    this.namespace ='api';
+    this.namespace = 'api';
 
     this.get('/transactions', () => {
       return this.schema.all('transaction');
     })
 
     this.post('/transactions', (schema, request) => {
-        const data = JSON.parse(request.requestBody)
-        return schema.create('transaction', data);
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data);
     })
 
   }
